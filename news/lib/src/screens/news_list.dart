109b81c852d21f416428a4fe7d823a065c3ff668
Nikola Curilovic <ncurilovic@gmail.com>
2018-07-24 15:09:45 +0200

    Add news list item widget

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../blocs/stories_provider.dart';
+import '../widgets/news_list_tile.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -28,7 +29,11 @@ class NewsList extends StatelessWidget {
         return ListView.builder(
           itemCount: snapshot.data.length,
           itemBuilder: (contex, int index) {
-            return Text('${snapshot.data[index]}');
+            bloc.fetchItem(snapshot.data[index]);
+
+            return NewsListTile(
+              itemId: snapshot.data[index],
+            );
           },
         );
       },
---
+++ news/lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+import 'dart:async';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+     stream: bloc.items,
+     builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+       if (!snapshot.hasData) {
+         return Text('Stream still loading');
+       }
+
+       return FutureBuilder(
+         future: snapshot.data[itemId],
+         builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+           if (!itemSnapshot.hasData) {
+             return Text('Still loading item $itemId');
+           }
+
+           return Text(itemSnapshot.data.title);
+         },
+       );
+     },
+    );
+  }
+}
