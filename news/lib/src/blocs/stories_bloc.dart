e68b220787f5dcc3aa4e18fca40460a2221837fe
Nikola Curilovic <ncurilovic@gmail.com>
2018-07-24 12:50:50 +0200

    Add items transformer, expose items stream to outside world :)

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -1,19 +1,40 @@
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
+import 'dart:async';
 
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
+
+  Observable<Map<int, Future<ItemModel>>> items;
 
   Observable<List<int>> get topIds => _topIds.stream;
+
+  Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
